010b7489c00bc24a6fb1e337db6b77c8a4864b32
arkaleta <arkaleta.inpg@gmail.com>
2017-05-16 22:06:52 +0200

    Dodanie funkcji z algorytmem Euklidesa
    Należy jeszcze przenieść wszystkie case do osobnych funkcji aby nie tworzyć zbyt dużo zmiennych.

--- Wolphram-omega v. 0.1.1.cpp
+++ Wolphram-omega v. 0.1.1.cpp
@@ -1,25 +1,58 @@
 #include <iostream>
 #include <cstdlib>
+
 using namespace std;
+
 float dodaj(float a, float b)
 {
     return a+b;
 }
+
+int NWD(int a, int b)
+{
+   if(a!=b)
+     return NWD(a>b?a-b:a,b>a?b-a:b);
+   return a;
+}
+
 int main()
 {
-    cout<<"Wybierz operacjê jak¹ chcesz wykonaæ wpisuj¹c odpowiedni¹ cyfrê wed³ug rozpisanych poni¿ej:"<<endl<<"1 - dodawanie"<<endl;
     int z;
-    cin>>z;
-    switch(z)
-    {
-    case 1:
-        {
-            float a, b, w;
-            cout<<"Podaj liczby, które chcesz dodaæ:"<<endl;
-            cin>>a>>b;
-            w=dodaj(a,b);
-            cout<<"Wynik to:"<<w<<endl;
-        }
+    
+    while(z != 3){
+        
+        cout<<"Wybierz operacjÃª jakÂ¹ chcesz wykonaÃ¦ wpisujÂ¹c odpowiedniÂ¹ cyfrÃª wedÂ³ug rozpisanych poniÂ¿ej:"<<endl
+            <<"1. Dodawanie"<<endl
+            <<"2. Najwiekszy Wspolny Dzielnik"<<endl
+            <<"3. Zakoncz program"<<endl;
+
+        cin>>z;
+
+        switch(z){
+
+            case 1:
+                float a, b, w;
+                cout<<"Podaj liczby, ktÃ³re chcesz dodaÃ¦:"<<endl;
+                cin>>a>>b;
+                w=dodaj(a,b);
+                cout<<"Wynik to:"<<w<<endl;
+                break;
+
+            case 2:
+                int c, d;
+                cout<<"Podaj dwie liczby: ";
+                cin>>a>>b;
+                cout<<"NWD("<<a<<","<<b<<") = "<<NWD(a,b)<<endl;  
+                break;
+            
+            case 3:
+                cout<<"Program zakonczy zadanie"<<endl;
+                return 0;
+                break;
+            
+            default: 
+                cout<<"Wprowadzono zla liczbe"<<endl; 
+                break;
     }
     return 0;
 }
